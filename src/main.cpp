230dbe02a99b5b00784fa88193db6795996c6c2e
Splin <denisvarava83@gmail.com>
2023-04-07 14:54:44 +0200

    New

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,19 @@
 #define SEALEVELPRESSURE_HPA (1013.25) // Задаем высоту
 #define postingInterval 330000         // интервал между отправками данных в миллисекундах (5 минут)
 
-unsigned long lastConnectionTime = 0; // время последней передачи данных
-
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include <Adafruit_BME280.h> // Подключаем библиотеку Adafruit_BME280
 #include <Adafruit_Sensor.h> // Подключаем библиотеку Adafruit_Sensor
 
-
 void ReadSensors();
 bool SendToNarodmon();
+void HTTP_init(void);
+void handleNotFound();
+void handleRoot();
+void handleSensors();
+void handle_Restart();
+
 // Your WiFi credentials.
 // Set password to "" for open networks.
 char ssid[] = "Ukrainer";
@@ -25,7 +29,10 @@ double bme280_temperature;
 double bme280_pressure;
 double bme280_humidity;
 
+unsigned long lastConnectionTime = 0; // время последней передачи данных
+
 Adafruit_BME280 bme; // Установка связи по интерфейсу I2C
+ESP8266WebServer HTTP(80);
 
 void setup()
 {
@@ -55,12 +62,18 @@ void setup()
       ; // Зацикливаем
   }
 
+  Serial.println("Start 2-WebServer");
+  HTTP_init();
+
   pinMode(LED_BUILTIN, OUTPUT);
   delay(1000);
 }
 
 void loop()
 {
+  HTTP.handleClient();
+  delay(1);
+
   if ((my_time + 10000) < millis())
   {
     ReadSensors();
@@ -71,11 +84,9 @@ void loop()
   if (millis() - lastConnectionTime > postingInterval)
   { // ждем 5 минут и отправляем
 
-    
-
     if (WiFi.status() == WL_CONNECTED)
     { // ну конечно если подключены
-      if (1)
+      if (SendToNarodmon)
       {
         lastConnectionTime = millis();
         Serial.println("Отправил на народмонитор");
@@ -132,4 +143,59 @@ void ReadSensors()
   Serial.println(bme280_temperature);
   Serial.println(bme280_pressure);
   Serial.println(bme280_humidity);
+}
+
+void HTTP_init(void)
+{
+  HTTP.onNotFound(handleNotFound);     // Сообщение если нет страницы. Попробуйте ввести http://192.168.0.101/restar?device=ok&test=1&led=on
+  HTTP.on("/", handleRoot);            // Главная страница http://192.168.0.101/
+  HTTP.on("/sensors", handleSensors);
+  HTTP.on("/restart", handle_Restart); // Перезагрузка модуля по запросу вида http://192.168.0.101/restart?device=ok
+  // Запускаем HTTP сервер
+  HTTP.begin();
+}
+
+// Ответ если страница не найдена
+void handleNotFound()
+{
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += HTTP.uri();
+  message += "\nMethod: ";
+  message += (HTTP.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += HTTP.args();
+  message += "\n";
+  for (uint8_t i = 0; i < HTTP.args(); i++)
+  {
+    message += " " + HTTP.argName(i) + ": " + HTTP.arg(i) + "\n";
+  }
+  HTTP.send(404, "text/plain", message);
+}
+
+// Ответ при обращении к основной странице
+void handleRoot()
+{
+  HTTP.send(200, "text/plain", "hello from esp8266!");
+  Serial.println("Обращение к корню сервера");
+}
+
+void handleSensors()
+{
+  String message="";
+  for (uint8_t i = 0; i < HTTP.args(); i++)
+  {
+    message += " " + HTTP.argName(i) + ": " + HTTP.arg(i) + "\n";
+  }
+  HTTP.send(404, "text/plain", message);
+  Serial.println("handleSensors");
+}
+
+// Перезагрузка модуля по запросу вида http://192.168.0.101/restart?device=ok
+void handle_Restart()
+{
+  String restart = HTTP.arg("device");
+  if (restart == "ok")
+    ESP.restart();
+  HTTP.send(200, "text/plain", "OK");
 }
